50fe9eb02be16bf987dfc8f002fdd7462d367fdb
cateadrian <77905982+cateadrian@users.noreply.github.com>
2024-07-30 15:51:44 -0600

    feat(board): add state getter function

--- lib/board.rb
+++ lib/board.rb
@@ -6,6 +6,6 @@ class Board
   end
 
   def state
-    puts @state
+    puts " #{@state[0]} | #{@state[1]} | #{@state[2]} \n----------- \n #{@state[3]} | #{@state[4]} | #{@state[5]} \n----------- \n #{@state[6]} | #{@state[7]} | #{@state[8]}"
   end
 end
--- main.rb
+++ main.rb
@@ -1,4 +1,8 @@
 require_relative "lib/player"
+require_relative "lib/board"
+
+board = Board.new
+board.state
 
 player_1 = Player.new("X")
 player_1.introduce
