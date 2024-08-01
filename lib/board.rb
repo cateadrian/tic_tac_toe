be9788990e1257f69e650f94923244bb2abc0c8a
cateadrian <77905982+cateadrian@users.noreply.github.com>
2024-08-01 15:55:53 -0600

    feat(board): Add player_move method that updates the board to reflect the current players move

--- lib/board.rb
+++ lib/board.rb
@@ -8,4 +8,8 @@ class Board
   def state
     puts " #{@state[0]} | #{@state[1]} | #{@state[2]} \n----------- \n #{@state[3]} | #{@state[4]} | #{@state[5]} \n----------- \n #{@state[6]} | #{@state[7]} | #{@state[8]}"
   end
+
+  def player_move(player, choice)
+    @state[choice] = player
+  end
 end
